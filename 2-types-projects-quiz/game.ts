ef12fdda366226c3cc9f4430559396ee6edee9b1
sdc02408 <sdc02408@naver.com>
2021-08-03 23:46:55 +0900

    [update] 기본 타입 연습퀴즈'

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,28 +1,49 @@
 /**
  * Let's make a game 🕹
  */
-// console.log(position); // { x: 0, y: 0}
-// move('up');
-// console.log(position); // { x: 0, y: 1}
-// move('down');
-// console.log(position); // { x: 0, y: 0}
-// move('left');
-// console.log(position); // { x: -1, y: 0}
-// move('right');
-// console.log(position); // { x: 0, y: 0}
 
-type Move ='up' | 'down' | 'left' | 'right'
-function move(moves: Move, a = 0, b =0): any{
-    switch(moves){
+const position = {x: 0, y: 0};
+
+function move(direction : 'up' | 'down' | 'left' | 'right'){
+    switch(direction){
         case 'up':
-            return a, b+1;
+            position.y +=1;
+            break;
         case 'down':
-            return a, b-1;
+            position.y -=1;
+            break;
         case 'left':
-            return a - 1 , b;
+            position.x -=1;
+            break;
         case 'right':
-            return a + 1, b;
+            position.x +=1;
+            break;
+        default:
+            throw new Error(`error : ${direction}`)
     }
 }
 
-move('up')
+console.log(position); // { x: 0, y: 0}
+move('up');
+console.log(position); // { x: 0, y: 1}
+move('down');
+console.log(position); // { x: 0, y: 0}
+move('left');
+console.log(position); // { x: -1, y: 0}
+move('right');
+console.log(position); // { x: 0, y: 0}
+
+// type Move ='up' | 'down' | 'left' | 'right'
+// function move(moves: Move, a = 0, b = 0): any{
+//     if(moves === 'up'){
+//         return a, b+1
+//     } else if (moves === 'down'){
+//         return a, b-1
+//     } else if(moves === 'left'){
+//         return a-1 , b;
+//     } else if (moves === 'right'){
+//         return a+1, b;
+//     }
+// }
+
+// move('up')
--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -20,6 +20,18 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+
+  function printLoginState(message: ResourceLoadState): void {
+    if(message.state == 'loading'){
+      console.log('👀 loading...')
+    } else if (message.state == 'success'){
+      console.log(message.response.body)
+    } else if (message.state == 'fail'){
+        console.log(message.reason)
+    }
+  }
+
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
